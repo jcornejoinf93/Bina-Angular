a2d4aa3b812bf4e85adff94cb5d2656601aa5c0f
Jorge <j.cornejosepulveda@gmail.com>
2021-11-02 11:04:46 -0300

    grilla resultados coin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResultsTableComponent } from './pages/results-table/results-table.component';
+import { DropCeroPipe } from './pipes/drop-cero.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResultsTableComponent,
+    DropCeroPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
